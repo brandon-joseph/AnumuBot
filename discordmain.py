9562b956552455ad0d5ccbd9044e1da36613263f
Brandon Joseph <brandonjgcjoseph@gmail.com>
2019-11-02 16:38:33 -0400

    Added memory checks

--- discordmain.py
+++ discordmain.py
@@ -115,6 +115,10 @@ class Music(commands.Cog):
         print(latest_file)
 
         clip = mp.VideoFileClip(latest_file)
+        statinfo = os.stat(latest_file).st_size
+        if (statinfo <= 8000000):
+            await ctx.send(file=discord.File(latest_file))
+            return
         # clip_resized = clip.resize(height=360)  # make the height 360px ( According to moviePy documenation The width is then computed so that the width/height ratio is conserved.)
         clip.write_videofile("movie_resized.mp4", bitrate="200k")
 
@@ -155,6 +159,11 @@ class Music(commands.Cog):
         print(latest_file)
 
         clip = mp.VideoFileClip(latest_file)
+        statinfo = os.stat(latest_file).st_size
+        if (statinfo <= 8000000):
+            await ctx.send("Title: " + submission.title)
+            await ctx.send(file=discord.File(latest_file))
+            return
         clip.write_videofile("movie_resized.mp4", bitrate="200k")
 
         await ctx.send("Title: " + submission.title)
